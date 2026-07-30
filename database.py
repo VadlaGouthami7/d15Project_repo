fd403b7dba9a5d72e9c045230d3ec0764fe2dc31
Gouthamivadla <gouthamivadla7@gmail.com>
2026-07-30 18:30:00 +0530

    Change DATABASE_URL to Aiven database connection
    Updated DATABASE_URL to use Aiven database credentials.

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "mysql+pymysql://root:gouthami%405f7@localhost:3306/project_db"
+# DATABASE_URL = "mysql+pymysql://root:gouthami%405f7@localhost:3306/project_db"
 #http://localhost:8000
+DATABASE_URL="mysql+pymysql://avnadmin:AVNS_Sc8fGmoHJQT-PEmh3pb@gouthami-7-gouthamivadla7-5fb0.j.aivencloud.com:28983/defaultdb"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
