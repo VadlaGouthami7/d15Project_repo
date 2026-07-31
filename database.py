7c7ffca35f519b052bab4e00914d31b736b3a956
Gouthamivadla <gouthamivadla7@gmail.com>
2026-07-31 20:43:22 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,9 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-# DATABASE_URL = "mysql+pymysql://root:gouthami%405f7@localhost:3306/project_db"
+
 #http://localhost:8000
-# DATABASE_URL="mysql+pymysql://avnadmin:AVNS_Sc8fGmoHJQT-PEmh3pb@gouthami-7-gouthamivadla7-5fb0.j.aivencloud.com:28983/defaultdb"
 load_dotenv()
 DATABASE_URL=os.getenv("DB_URL")
 engine = create_engine(DATABASE_URL)
