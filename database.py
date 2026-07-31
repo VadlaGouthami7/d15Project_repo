bd927826ae240933b946d3d6c9849e830f4a4a30
VadlaGouthami7 <gouthamivadla7@gmail.com>
2026-07-31 20:36:52 +0530

    modified database.py

--- database.py
+++ database.py
@@ -10,8 +10,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # DATABASE_URL = "mysql+pymysql://root:gouthami%405f7@localhost:3306/project_db"
 #http://localhost:8000
 # DATABASE_URL="mysql+pymysql://avnadmin:AVNS_Sc8fGmoHJQT-PEmh3pb@gouthami-7-gouthamivadla7-5fb0.j.aivencloud.com:28983/defaultdb"
+load_dotenv()
 DATABASE_URL=os.getenv("DB_URL")
-engine = create_engine(DB_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
